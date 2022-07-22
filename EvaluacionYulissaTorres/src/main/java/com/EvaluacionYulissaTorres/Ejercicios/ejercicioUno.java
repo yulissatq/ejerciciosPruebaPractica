81a36938383f54757b016c7beca0e8f91c8f38e1
Yulissa Stefania <33470004+yulissatq@users.noreply.github.com>
2022-07-22 10:59:44 -0500

    Add files via upload

---
+++ EvaluacionYulissaTorres/src/main/java/com/EvaluacionYulissaTorres/Ejercicios/ejercicioUno.java
@@ -0,0 +1,30 @@
+package com.EvaluacionYulissaTorres.Ejercicios;
+
+
+
+public class ejercicioUno {
+	public static void main(String[] args) {
+                      
+        
+        int[] numeros = new int[100];
+        for ( int i= 0; i < numeros.length; i++) {
+        	
+        	if(numeros[i]%3!=0 && numeros[i]%5!=0){
+        		numeros[i] = i+1;
+        		System.out.println(numeros[i]);
+        		
+        	} else if (numeros[i]%3==0 && numeros[i]%5==0){
+        		numeros[i] = i+1;
+        		System.out.println(numeros[i] + "SOMOS BA");
+        	} else if(numeros[i]%3==0){
+            		numeros[i] = i+1;
+                    System.out.println(numeros[i] + "SOMOS");
+            } else if (numeros[i]%5==0) {
+            	numeros[i] = i+1;
+            	System.out.println(numeros[i] + "BA");
+                    	
+                    }
+        }
+        }
+	
+}
